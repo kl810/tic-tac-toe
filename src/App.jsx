a1c930ea5c428c9b14393221e0ccda861c713630
Kye-Lee <kyelee.low@gmail.com>
2025-01-18 19:30:01 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Player from "./components/Player";
+import Gameboard from "./components/Gameboard";
+import Log from "./components/Log";
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+      <Log />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleEditing() {
+        setIsEditing((isEditing) => !isEditing);
+    }
+
+    function handleChange(e) {
+        setPlayerName(e.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>; 
+    let btnCaption = "Edit"
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
+
+        btnCaption = "Save"
+    }
+
+    return (
+        <li className={isActive ? "active" : undefined}>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditing}>{btnCaption}</button>
+        </li>
+    )
+
+}          
