5d60b32981c88c13418cc44e3f2a562f0b2cdb90
Kye-Lee <kyelee.low@gmail.com>
2025-01-18 21:11:40 +0000

    alternative to lifting state - fn handlePlayerNameChange update object key-value with newName from Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ function deriveActivePlayer(gameTurns) { //prop derived from line 20
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X : 'Player 1',
+    O : 'Player 2',
+  })
   const [gameTurns, setGameTurns] = useState([])
   // const [activePlayer, setActivePlayer] = useState('X')
 
@@ -54,7 +58,7 @@ function App() {
         firstSquareSymbol === secondSquareSymbol && 
         firstSquareSymbol === thirdSquareSymbol
       ) {
-        winner = firstSquareSymbol;
+        winner = players[firstSquareSymbol]; //derive name from line 27 state
       }
   }
 
@@ -78,12 +82,31 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player 
+            initialName="Player 1" 
+            symbol="X" 
+            isActive={activePlayer === 'X'} 
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player 
+            initialName="Player 2" 
+            symbol="O" 
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <Gameboard 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
     function handleEditing() {
         setIsEditing((isEditing) => !isEditing);
+
+        if (isEditing){
+            onChangeName(symbol, playerName); //from playerName state above
+        }    
     }
 
     function handleChange(e) {
